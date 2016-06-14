07809f60c2071e1a27e7229b81181e3c146fa889
Raül Pérez <repejota@gmail.com>
2016-06-14 17:01:38 +0200

    Basic get handler test skel

--- basic_test.go
+++ basic_test.go
@@ -3,3 +3,24 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 package main
+
+import (
+	"testing"
+
+	"github.com/nats-io/nats"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestGetHandler(t *testing.T) {
+	setupNats()
+	n.Subscribe("config.get.postgres", func(msg *nats.Msg) {
+		n.Publish(msg.Reply, []byte(`{"names":["users","datacenters","datacenters","services"],"password":"","url":"postgres://postgres@127.0.0.1","user":""}`))
+	})
+	setupPg()
+	startHandler()
+
+	Convey("Scenario: getting a group", t, func() {
+
+	})
+}
